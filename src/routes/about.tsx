51a80da87dd68e618561e583ed3bbb039ec1bda0
Pavel Sauchanka <pavel.sauchanka@innowise.com>
2025-06-20 11:21:34 +0200

    init dev branch

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -9,8 +9,7 @@ export const Route = createFileRoute('/about')({
 function About() {
 
 
-  return <div className="p-2">Hello from About!
-  </div>
+  return <div className="p-2">Hello from About!</div>
 }
 
 
